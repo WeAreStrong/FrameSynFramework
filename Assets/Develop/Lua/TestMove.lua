ce3b73f5f2620952c492d15b86963839f3a3efc7
Fei Xu <fei.xu@dena.com>
2017-07-25 15:30:15 +0800

    格式化msg

--- Assets/Develop/Lua/TestMove.lua
+++ Assets/Develop/Lua/TestMove.lua
@@ -3,15 +3,19 @@ module("TestMove", package.seeall)
 function Init()
     Socket:OnPushEvent("onChat", function(msg)
         SocketQueue:Push(function()
-        	for k, v in pairs(msg) do
-        		error(k, v);
-        	end
+            error(msg);
         end);
     end);
 end
 
 function SendMove(point)
 	local str = string.format('{"x":%d,"y":%d}', point.x, point.y);
+    local param = 
+    {
+        content = str,
+        target = '*',
+        rid = 'mobage_cn|debug|world|1'
+    }
 	print(str);
-	Socket:Request("chat.chatHandler.send", str);
+	Socket:Request("chat.chatHandler.send", param);
 end
